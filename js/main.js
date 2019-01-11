ffe466636b29a3980e4ba36594b911b5ea80b67a
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-11 14:07:42 -0600

    Mouse Toggle Complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const button = document.querySelector("#btn");
 const mouse = document.querySelector("#mouse");
+let display = false;
 
 button.addEventListener("click", () => {
-  mouse.style.opacity = 1;
+  display = !display;
+
+  if (display === false) {
+    mouse.style.opacity = 0;
+  } else {
+    mouse.style.opacity = 1;
+  }
 });
