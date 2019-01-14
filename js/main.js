ac3abbc8eadd2f309769c5ed875e91495f08ad72
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-14 13:22:41 -0600

    Testimonials Section Completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const button = document.querySelector("#btn");
 const mouse = document.querySelector("#mouse");
 const services = document.querySelector("#services");
+const reviews = document.querySelector("#reviews");
 let display = false;
 
 button.addEventListener("click", () => {
@@ -9,8 +10,10 @@ button.addEventListener("click", () => {
   if (display === false) {
     mouse.style.opacity = 0;
     services.style.display = "none";
+    reviews.style.display = "none";
   } else {
     mouse.style.opacity = 1;
     services.style.display = "flex";
+    reviews.style.display = "flex";
   }
 });
